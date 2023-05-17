c6feaecce87e6373b8ade13d149742abfac00e9f
seongwon-ha <djfqks22@naver.com>
2023-05-17 10:39:52 +0900

    useState & useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
+
+  const addHandler = () => {
+    setNum(num + 1);
+  }
+
+  const subHandler = () => {
+    setNum(num - 1);
+  }
+
+  useEffect(()=> {
+  },[num])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => addHandler()}>up</button>
+      {num}
+      <button onClick={() => subHandler()}>down</button>
     </div>
   );
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+/**
+ * 타이틀 컴포넌트입니다.
+ */
+const Title = (props) => {
+    return <h1>{props.title}</h1>;
+    
+};
+
+export default Title
